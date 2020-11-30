38eb2df65a7b3e3693e60d1cd2af246898a034d0
Ibrahim Faruqi <izfaruqi@gmail.com>
2020-11-30 22:52:45 +0700

    Clean up apis

--- src/apis/jikan.js
+++ src/apis/jikan.js
@@ -1,17 +1,20 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "https://api.themoviedb.org/3",
+    baseURL: "https://api.jikan.moe/v3",
 })
 
 export async function searchAnime(query, page){
-    console.log(process.env.REACT_APP_TMDB_API_KEY)
-    let result = await api.get('/search/anime', {params: {api_key: process.env.REACT_APP_TMDB_API_KEY, query: query, page: page}})
+    let result = await api.get('/search/anime', {params: {q: query, page: page}})
     return result.data
 }
 
 export async function getTopAnime(page){
-    console.log(process.env.REACT_APP_TMDB_API_KEY)
-    let result = await api.get('/top/anime/' + page, {params: {api_key: process.env.REACT_APP_TMDB_API_KEY}})
+    let result = await api.get('/top/anime/' + page, {params: {}})
+    return result.data
+}
+
+export async function getAnimeById(id){
+    let result = await api.get('/anime/' + id, {params: {}})
     return result.data
 }
--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -5,19 +5,16 @@ const api = axios.create({
 })
 
 export async function searchMovies(query, page){
-    console.log(process.env.REACT_APP_TMDB_API_KEY)
     let result = await api.get('/search/movie', {params: {api_key: process.env.REACT_APP_TMDB_API_KEY, query: query, page: page}})
     return result.data
 }
 
 export async function getTrendingMovies(page){
-    console.log(process.env.REACT_APP_TMDB_API_KEY)
     let result = await api.get('/trending/movie/week', {params: {api_key: process.env.REACT_APP_TMDB_API_KEY, page: page}})
     return result.data
 }
 
 export async function getMovieById(id){
-    console.log(process.env.REACT_APP_TMDB_API_KEY)
     let result = await api.get('/movie/' + id, {params: {api_key: process.env.REACT_APP_TMDB_API_KEY}})
     return result.data
 }
